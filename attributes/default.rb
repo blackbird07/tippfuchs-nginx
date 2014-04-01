30f94c55618dc965946bdb5994d3b32fbaec21bc
Enrico Mrass <enrico.mrass@googlemail.com>
2014-04-01 22:54:32 +0200

    Disable default site by default

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,4 @@
-default['nginx']['install_method'] = 'package'
-default['nginx']['repo_source']    = 'nginx'    # install nginx from PPA
+default['nginx']['install_method']    = 'package'
+default['nginx']['repo_source']       = 'nginx'    # install nginx from PPA
+
+node['nginx']['default_site_enabled'] = false
