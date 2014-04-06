9ac29294fcf8b533d77a41a3b29a517bc8552807
Enrico Mrass <github@emrass.de>
2014-04-06 11:51:45 +0200

    Adding configuration

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,10 @@ default['nginx']['install_method']       = 'package'
 default['nginx']['repo_source']          = 'nginx'    # install nginx from PPA
 
 default['nginx']['default_site_enabled'] = false
+
+default['nginx']['server_tokens']        = 'off'
+default['nginx']['keepalive_timeout']    = 65
+
+# TODO
+#ssl_session_cache   shared:SSL:10m;
+#ssl_session_timeout 10m;
