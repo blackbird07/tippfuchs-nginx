34f9125e59c180fc5c83988a9d725d43dfef523a
Enrico Mrass <github@emrass.de>
2014-04-23 21:13:06 +0200

    Move version dependency from Berksfile to metadata

--- Berksfile
+++ Berksfile
@@ -2,5 +2,4 @@ source 'https://api.berkshelf.com'
 
 metadata
 
-cookbook 'nginx',              '~> 2.6.0'
-cookbook 'tippfuchs-fail2ban', '~> 0.1.0', github: 'emrass/tippfuchs-fail2ban'
+cookbook 'tippfuchs-fail2ban', github: 'emrass/tippfuchs-fail2ban'
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'enrico.mrass@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures tippfuchs-nginx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.9'
+version          '1.0.10'
 
-depends 'nginx'
-depends 'tippfuchs-fail2ban'
+depends 'nginx',              '~> 2.6.0'
+depends 'tippfuchs-fail2ban', '~> 0.1.0'
